73e765eb9820921bdf2a02ff4f99defd018e39de
GiacomoRobino <30469325+GiacomoRobino@users.noreply.github.com>
2022-04-19 19:04:21 +0200

    Add header animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { gsap, Power4 } from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('aboutMe') aboutMe: any;
+  @ViewChild('contactMe') contactMe: any;
   title = 'portfolio-fast';
+
+  ngAfterViewInit(){
+    this.moveHeaderButton();
+  }
+
+  moveHeaderButton(){
+    let tl = gsap.timeline();
+    let t2 = gsap.timeline();
+    tl.from(this.aboutMe.nativeElement, {duration: 1, y: -1000, ease: Power4.easeOut});
+    t2.from(this.contactMe.nativeElement, {duration: 1, y: -1000, ease: Power4.easeOut});
+  }
 }
