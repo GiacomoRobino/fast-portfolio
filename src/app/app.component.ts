4fc4a63774a31a763fffa29e4f4d5f16d4ac1664
GiacomoRobino <30469325+GiacomoRobino@users.noreply.github.com>
2022-07-11 17:38:22 +0200

    animated border for header button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,38 @@
-import { AfterViewInit, Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ContactMeComponent } from './components/contact-me/contact-me.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { gsap, Power4 } from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 @Injectable()
-
 export class AppComponent implements AfterViewInit {
   @ViewChild('aboutMeButton') aboutMeButton: any;
-  @ViewChildren('aboutMeComponent') aboutMeComponent !: QueryList<AboutMeComponent>;
-  @ViewChild('contactMeButton') contactMeButton: any;
-  @ViewChildren('contactMeComponent') contactMeComponent !: QueryList<ContactMeComponent>;
+  @ViewChildren('aboutMeComponent')
+  aboutMeComponent!: QueryList<AboutMeComponent>;
+  @ViewChild('contactMeButtonContainer') contactMeButtonContainer: any;
+  @ViewChildren('contactMeButton') contactMeButtonList!: QueryList<any>;
+  @ViewChildren('contactMeComponent')
+  contactMeComponent!: QueryList<ContactMeComponent>;
   @ViewChild('introductionButton') projectsButton: any;
-  @ViewChildren('introductionComponent') projectsComponent !: QueryList<ProjectsComponent>;
+  @ViewChildren('introductionComponent')
+  projectsComponent!: QueryList<ProjectsComponent>;
   @ViewChild('componentContainer') componentContainer: any;
   public headerVisible = false;
-  public contactMeButtonText = "";
-  public fullText = "Contact Me";
+  public contactMeButtonText = '';
+  public fullText = 'Contact Me';
   private timeToWrite = 1000.0;
   private specialCaractersMultipliers: { [key: string]: number } = {
     '.': 500.0,
@@ -31,49 +40,63 @@ export class AppComponent implements AfterViewInit {
   };
   private interruptWriting = false;
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {}
 
-  }
-
-  public modules : {[key:string]:any} = {}
-  public buttons : {[key:string]:any} = {}
-  public visible : {[key:string]:boolean} = {contactMe : false, aboutMe : true, introduction : false}
-  public initiated = false
+  public modules: { [key: string]: any } = {};
+  public buttons: { [key: string]: any } = {};
+  public visible: { [key: string]: boolean } = {
+    contactMe: false,
+    aboutMe: true,
+    introduction: false,
+  };
+  public initiated = false;
   title = 'portfolio-fast';
 
+  ngAfterViewInit() {
+    this.modules = {
+      aboutMe: this.aboutMeComponent.first,
+      contactMe: undefined,
+    };
+    this.buttons = {
+      aboutMe: this.aboutMeButton
+    };
 
-  ngAfterViewInit(){
-
-    this.modules = { aboutMe : this.aboutMeComponent.first, contactMe: undefined}
-    this.buttons = { aboutMe : this.aboutMeButton, contactMe: this.contactMeButton}
-
-    this.aboutMeComponent.changes.subscribe((comps: QueryList <any>) =>{
-      this.modules.aboutMe = comps.first;});
-    this.contactMeComponent.changes.subscribe((comps: QueryList <any>) =>{
-      this.modules.contactMe = comps.first;});
+    this.aboutMeComponent.changes.subscribe((comps: QueryList<any>) => {
+      this.modules.aboutMe = comps.first;
+    });
+    this.contactMeComponent.changes.subscribe((comps: QueryList<any>) => {
+      this.modules.contactMe = comps.first;
+    });
+    this.contactMeButtonList.changes.subscribe((comps: QueryList<any>) => {
+      {
+        const tl = gsap.timeline();
+        tl.to(comps.first.nativeElement, {
+          duration: 2,
+          borderColor: 'white',
+        }).then(() => this.writeText());
+      }
+    });
   }
 
-  click(element: string){
+  click(element: string) {
     const currentVisibleComponent = this.getVisibleComponent();
-    this.modules[currentVisibleComponent].clickClose().then( (data : string) => {
-      this.expandElement(element)
-    })
+    this.modules[currentVisibleComponent].clickClose().then((data: string) => {
+      this.expandElement(element);
+    });
   }
 
-  expandElement(elementToExpand : string){
-          this.visible[this.getVisibleComponent()] = false
-          this.visible[elementToExpand] = true
-    
+  expandElement(elementToExpand: string) {
+    this.visible[this.getVisibleComponent()] = false;
+    this.visible[elementToExpand] = true;
   }
 
-  getVisibleComponent() : string{
+  getVisibleComponent(): string {
     const keys = Object.keys(this.visible);
-    return keys.filter(key => this.visible[key])[0]
+    return keys.filter((key) => this.visible[key])[0];
   }
 
-  finishedIntro(){
+  finishedIntro() {
     this.headerVisible = true;
-    this.writeText()
   }
 
   writeText(timeToWrite = -1): any {
@@ -86,17 +109,28 @@ export class AppComponent implements AfterViewInit {
         this.contactMeButtonText.length < this.fullText.length &&
         !this.interruptWriting
       ) {
-        this.contactMeButtonText = this.addOneLetter(this.contactMeButtonText, this.fullText);
-        if (this.specialCaractersMultipliers[this.contactMeButtonText.slice(-1)]) {
+        this.contactMeButtonText = this.addOneLetter(
+          this.contactMeButtonText,
+          this.fullText
+        );
+        if (
+          this.specialCaractersMultipliers[this.contactMeButtonText.slice(-1)]
+        ) {
           timeToWrite =
             timeToWrite *
-            this.specialCaractersMultipliers[this.contactMeButtonText.slice(-1)];
+            this.specialCaractersMultipliers[
+              this.contactMeButtonText.slice(-1)
+            ];
         } else if (
-          this.specialCaractersMultipliers[this.contactMeButtonText.slice(-2, -1)]
+          this.specialCaractersMultipliers[
+            this.contactMeButtonText.slice(-2, -1)
+          ]
         ) {
           timeToWrite =
             timeToWrite /
-            this.specialCaractersMultipliers[this.contactMeButtonText.slice(-2, -1)];
+            this.specialCaractersMultipliers[
+              this.contactMeButtonText.slice(-2, -1)
+            ];
         }
         setTimeout(() => {
           this.writeText(timeToWrite).then(resolve);
